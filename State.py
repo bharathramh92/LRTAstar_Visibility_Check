99ae691e08469f4a2958c4e2bfa4257c0df3c52f
bharath <bharathramh92@gmail.com>
2015-11-18 23:12:43 -0500

    updated successor list generation

--- State.py
+++ State.py
@@ -1,17 +1,10 @@
-STEP_COST = 1
-
-
 class State:
 
-    def __init__(self, position, resolution, obstacle_list, GOAL_STATE=None, operation='-', parent=None):
+    def __init__(self, position, GOAL_STATE=None):
         self.position = tuple(position)
         self.GOAL_STATE = GOAL_STATE
-        self.obstacle_list = obstacle_list
-        self.parent = parent
-        self.g, self.h = 0, 0
+        self.h = 0
         self.successor_list = []
-        self.operation = operation
-        self.resolution = resolution
         if GOAL_STATE is not None:
             self.heuristics()
 
@@ -20,38 +13,16 @@ class State:
         self.h += abs(self.position[0]-self.GOAL_STATE.position[0]) + \
                   abs(self.position[1]-self.GOAL_STATE.position[1])
 
-    def get_f(self):
-        return self.g + self.h
+    def is_goal(self):
+        return self.position == self.GOAL_STATE.position
 
-    def successor(self):
-        #Generates possible childs by moving empty cell to a maximum of 4 possibilities
-        i_row, y_col = self.position[0], self.position[1]
-        if i_row - 1 >= 0:     #left
-            position = tuple((i_row - 1, y_col))
-            # print('left' , position)
-            if position not in self.obstacle_list:
-                self.successor_list.append(State(position, self.resolution, self.obstacle_list, self.GOAL_STATE, operation='Left'))
-        if i_row + 1 < self.resolution:    #right
-            position = tuple((i_row + 1, y_col))
-            # print('right', position)
-            if position not in self.obstacle_list:
-                self.successor_list.append(State(position, self.resolution, self.obstacle_list, self.GOAL_STATE, operation='Right'))
-        if y_col - 1 >= 0:    #down
-            position = tuple((i_row, y_col - 1))
-            # print('up' , position)
-            if position not in self.obstacle_list:
-                self.successor_list.append(State(position, self.resolution, self.obstacle_list, self.GOAL_STATE, operation='Down'))
-        if y_col + 1 < self.resolution:    #up
-            position = tuple((i_row, y_col + 1))
-            # print('down', position)
-            if position not in self.obstacle_list:
-                self.successor_list.append(State(position, self.resolution, self.obstacle_list, self.GOAL_STATE, operation='Up'))
-        # if len(self.successor_list) < 4:
-        #     print("Generated %d successors." % len(self.successor_list))
+    def successor(self, visible_vertices):
+        for vertex in visible_vertices:
+            self.successor_list.append(State(vertex, self.GOAL_STATE))
         return self.successor_list
 
     def __lt__(self, other):
-        return self.get_f() < other.get_f()
+        return self.h < other.h
 
     def __repr__(self):
-        return 'State : %s, h : %d operation : %s\n' %(self.position, self.h, self.operation)
+        return 'State : %s, h : %d\n' % (self.position, self.h)
