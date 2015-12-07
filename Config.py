f54acaf8b661a914181716b2c46edba05027eaf6
bharath <bharathramh92@gmail.com>
2015-12-07 15:03:53 -0500

    added animation

--- Config.py
+++ Config.py
@@ -77,21 +77,19 @@ class Environment:
 
     def draw_env(self, path, key_xy,k_value ):
         fig, ax = plt.subplots()
-        # fig, ax = plt.axes()
         x_path, y_path = [], []
 
         for ls in path:
             x_path.append(key_xy(ls)[0])
             y_path.append(key_xy(ls)[1])
 
-
         colors = 100*np.random.rand(len(self.plot_obstacles_polygon))
         p = PatchCollection(self.plot_obstacles_polygon, cmap=matplotlib.cm.jet, alpha=0.4)
         p.set_array(np.array(colors))
         ax.add_collection(p)
         plt.colorbar(p)
-        print("x_path is " + str(x_path[0])+" "+str(x_path[1]))
-        print("y_path is " + str(y_path))
+        # print("x_path is " + str(x_path[0])+" "+str(x_path[1]))
+        # print("y_path is " + str(y_path))
 
         plt.plot([self.initial_state[0]], [self.initial_state[1]], 'bs', self.goal_state[0], self.goal_state[1], 'g^')
             # ,
@@ -106,9 +104,58 @@ class Environment:
 
         fig.savefig("figure"+ str(k_value),format = 'jpeg',dpi=fig.dpi)
 
-        k_value+=1
+    def animate_path(self, path, key_xy):
+        fig, ax = plt.subplots()
 
-    
+        colors = 100*np.random.rand(len(self.plot_obstacles_polygon))
+        p = PatchCollection(self.plot_obstacles_polygon, cmap=matplotlib.cm.jet, alpha=0.4)
+        p.set_array(np.array(colors))
+        ax.add_collection(p)
+        plt.colorbar(p)
+
+        plt.plot([self.initial_state[0]], [self.initial_state[1]], 'bs', self.goal_state[0], self.goal_state[1], 'g^')
+        plt.axis([0, self.resolution, 0, self.resolution])
+
+        x_0, y_0 = path[0].position[0], path[0].position[1]
+        x_1, y_1 = path[0 + 1].position[0], path[0 + 1].position[1]
+        print((x_0, x_1), (y_0, y_1))
+        # line, = ax.plot((x_0, x_1), (y_0, y_1))
+        dx, dy = x_1 - x_0, y_0 - y_1
+        qv = ax.quiver(x_0, y_0, dx, dy, angles='xy',scale_units='xy',scale=1)
+        # arrow = ax.arrow(x_0, y_0, dx, dy, fc="k", ec="k", head_width=1.55, head_length=1.1)
+        def animate(i):
+            x_init, y_init = path[i].position[0], path[i].position[1]
+            x_f, y_f = path[i + 1].position[0], path[i + 1].position[1]
+            dx, dy = x_f - x_init, y_f - y_init
+            # line.set_ydata((y_init, y_f))
+            # line.set_xdata((x_init, x_f))
+            # ax.arrow.remove(qv)
+            # qv.set_offsets(qv.get_offsets() * (dx, dy))
+            # qv.remove()
+            qv.set_UVC(np.array(dx), np.array(dy))
+            # print(qv.C)
+            # print(qv.U, " ", qv.V, " ", type(qv.U))
+            # print(np.array(dx))
+            # qv.U = np.array(dx)
+            # qv.V = np.array(y_f)
+            qv.set_offsets((x_init, y_init))
+            # print(qv.X, qv.Y, qv.U, qv.V)
+            # qv.X = x_init
+            # qv.
+            # qv = ax.quiver(x_init, y_init, dx, dy, angles='xy',scale_units='xy',scale=1)
+            return qv
+
+        anim = animation.FuncAnimation(fig, animate, frames=range(0, len(path)-1), interval=500)
+        # movieWriter = matplotlib.animation.MovieWriter(fps=5, codec=None, bitrate=None, extra_args=None, metadata=None)
+        # mywriter = animation.FFMpegWriter()
+        # anim.save('mymovie.mp4', writer=mywriter)
+
+        # FFMpegWriter = animation.writers['avconv']
+        # metadata = dict(title='Movie Test', artist='Matplotlib',
+        #                 comment='Movie support!')
+        # writer = FFMpegWriter(fps=15, metadata=metadata)
+        # anim.save('demoanimation.gif', writer='avconf', fps=4, dpi=1)
+        plt.show()
 
     def get_apprx_visible_vertices(self, xy_robot):
         if self.is_point_inside(xy_robot):
--- Pilot.py
+++ Pilot.py
@@ -6,13 +6,10 @@ import math
 
 def main():
 
-    env = Environment("Environment1.json")
+    env = Environment("Environments/Environment1.json")
     # obstacle_list = set()  #Known obstacles for the robot
     print(env.get_apprx_visible_vertices(env.initial_state))
     traversal_path = []
-
-
-    # def local_search(present_state):
     goal_state = State(env.goal_state)
     initial_state = State(env.initial_state, GOAL_STATE=goal_state)
     traversed = {}
@@ -44,92 +41,15 @@ def main():
     print("This is traversal")
     print(traversal_path)
 
-    temp_path=[]
-    for i in range(0,len(traversal_path)-1):
-        temp_path.append(traversal_path[i])
-        temp_path.append(traversal_path[i+1])
-        env.draw_env(temp_path,lambda x: x.position,i)
-        temp_path[:] = []
-
-
-
-    # for current_path in traversal_path:
-    #     print("Current path")
-    #
-    #     temp_path.append(current_path)
-    #
-    #     env.draw_env(temp_path, lambda x: x.position)
-
-    # local_search(env.initial_state)
-
-    # def a_star(present_state):
-    #     open_set_pq, open_set, closed_set = PriorityQueue(), {}, {}
-
-    # goal_state = State(env.goal_state, env.resolution, obstacle_list)
-    # initial_state = State(present_state, env.resolution, obstacle_list, GOAL_STATE=goal_state)
-        # open_set_pq.put(initial_state)
-        # open_set[initial_state.position] = initial_state
-        # state_examinations, state_generated = 0, 1
-        # while True:
-        #     if not open_set_pq.empty():
-        #         current_state = open_set_pq.get()
-        #         state_examinations += 1
-        #         open_set.pop(current_state.position)
-        #         closed_set[current_state.position] = current_state
-        #     else:
-        #         print('No solution')
-        #         exit()
-        #     if current_state.position == goal_state.position:
-        #         return current_state, state_examinations, state_generated
-        #     else:
-        #         for x in current_state.successor():
-        #             if x.position in closed_set or x.position in obstacle_list:
-        #                 continue
-        #             g_cost = current_state.g + 1      #STEP_COST --> 1
-        #             if x.position not in open_set or g_cost < open_set[x.position].g:
-        #                 x.g = g_cost
-        #                 x.parent = current_state
-        #                 if x.position not in open_set:
-        #                     state_generated += 1
-        #                     open_set[x.position] = x
-        #                     open_set_pq.put(x)
-
-    # def lrta_star(present_state, total_restarts_lrta_star):
-    #     print("lrta_star restart number: ", total_restarts_lrta_star)
-    #
-    #     def tree_traversal():
-    #         current_state, state_examinations, state_generated = a_star(present_state)
-    #         path = []
-    #         while current_state.parent is not None:
-    #             path.append(current_state)
-    #             current_state = current_state.parent
-    #         path.reverse()
-    #         return path, state_generated, state_examinations
-    #     path, state_generated, state_examinations = tree_traversal()
-    #
-    #     def go_to_next_position(i):
-    #         # print("i: %d path %s" %(i, path))
-    #         current_position_traversed = path[i]
-    #         x, y = current_position_traversed.position[0], current_position_traversed.position[1]
-    #         print(x, y, env.is_point_inside_box(x, y))
-    #         if env.is_point_inside_box(x, y) or current_position_traversed.position == tuple(env.goal_state):
-    #             return current_position_traversed
-    #         else:
-    #             traversal_path.append(current_position_traversed)
-    #             return go_to_next_position(i + 1)
-    #     current_position = go_to_next_position(0)
-    #     if current_position.position == tuple(env.goal_state):
-    #         print(traversal_path)
-    #         env.draw_env(traversal_path)
-    #         # print('Total steps were %d, generated %d and expanded %d ' % (len(path), state_generated, state_examinations))
-    #
-    #     else:
-    #         obstacle_list.add(current_position.position)
-    #         print("obstacle list is ", obstacle_list)
-    #         lrta_star(current_position.parent.position, total_restarts_lrta_star + 1)
-    #
-    # lrta_star(env.initial_state, 0)
-    # print(env.internal_is_point_inside(16, 13))
+    # temp_path = []
+    # for i in range(0, len(traversal_path)-1):
+    #     temp_path.append(traversal_path[i])
+    #     temp_path.append(traversal_path[i+1])
+    #     env.draw_env(temp_path,lambda x: x.position,i)
+    #     temp_path.clear()
+    # traversal_path
+    env.animate_path(traversal_path,lambda x: x.position)
+    print("Finished saving figure")
 
 if __name__ == '__main__':
     main()
